eed8b5e2c0d96c9ed48ddd9e1cc276ddf09d915c
teja1297 <89035725+teja1297@users.noreply.github.com>
2022-04-05 11:04:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const indexRouter = require('./routes/index');
 const SWAGGER = require("./config/swagger.js");
+var cors = require('cors');
 
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
@@ -23,11 +24,18 @@ const swaggerOptions = {
     },
     apis: SWAGGER.apis,
 };
+app.use(cors());
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With ,Content-Type, Accept");
+    next();
+});
 
 const specs = swaggerJsDoc(swaggerOptions);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use('/', indexRouter);
+
 
 app.listen(port, () => {
 	console.log("Server is running on port:" + port);
